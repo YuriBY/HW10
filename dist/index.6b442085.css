84c47e57768287a4696ecbcae3a9e033215eb71c
YuriBY <vv0325056@gmail.com>
2023-04-10 22:30:13 +0300

    NPM

---
+++ dist/index.6b442085.css
@@ -0,0 +1,29 @@
+@font-face {
+  font-family: Raleway Regular;
+  font-style: normal;
+  font-weight: normal;
+  src: local(Raleway Regular), local(Raleway-Regular), url("Raleway-Regular.6ba3ca7a.ttf") format("ttf");
+}
+
+@font-face {
+  font-family: Hind Siliguri;
+  font-style: normal;
+  font-weight: normal;
+  src: local(Hind Siliguri), local(Hind-Siliguri), url("HindSiliguri-Regular.1c8bd495.ttf") format("ttf");
+}
+
+@font-face {
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  src: local(Montserrat), local(Montserrat), url("Montserrat-Regular.0c6cfc8b.ttf") format("ttf");
+}
+
+@font-face {
+  font-family: Spectral SC;
+  font-style: normal;
+  font-weight: normal;
+  src: local(Spectral SC), local(Spectral SC), url("SpectralSC-Regular.144cdc6e.ttf") format("ttf");
+}
+
+/*# sourceMappingURL=index.6b442085.css.map */
